2210e4d8e5a39defc747b9e93a515a6e27709a26
cindysoares <cindy.as@gmail.com>
2020-06-07 21:48:10 -0300

    configuring sequelize and seeds

--- server/seeders/seed-farmers.js
+++ server/seeders/seed-farmers.js
@@ -1,16 +1,22 @@
 module.exports = {
-    up: (queryInterface, Sequelize) => queryInterface.bulkInsert('farmer', 
+    up: (queryInterface, Sequelize) => queryInterface.bulkInsert('farmers', 
       [
         {
-            name: 'Farmer 123'
+            name: 'Farmer 123',
+            createdAt: new Date(),
+            updatedAt: new Date()
         },
         {
-            name: 'Farmer 456'
+            name: 'Farmer 456',
+            createdAt: new Date(),
+            updatedAt: new Date()
         },
         {
-            name: 'XYZ'
+            name: 'XYZ',
+            createdAt: new Date(),
+            updatedAt: new Date()
         }
       ], {}),
   
-    down: (queryInterface) => queryInterface.bulkDelete('farmer', null, {}),
+    down: (queryInterface) => queryInterface.bulkDelete('farmers', null, {}),
   };
