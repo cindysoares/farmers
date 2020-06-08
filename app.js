28bba6567efa5d69fa6287e0c00ee88db3e2c8c9
cindysoares <cindy.as@gmail.com>
2020-06-08 09:08:59 -0300

    routing / to index.html

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const path = require('path');
 
 const app = express()
 const bodyParser = require('body-parser')
@@ -6,10 +7,12 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to farmers catalog." })
-})
+app.use(express.static(path.join(__dirname, 'dist/farmers')));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/farmers/index.html'));
+});
+  
 require("./server/routes/farmer.routes")(app)
 
 module.exports = app
